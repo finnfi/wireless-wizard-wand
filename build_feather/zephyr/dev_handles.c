16a682faede2a7f0c510e25431e75e4f662c1799
Finn Gross Maurer <finngm@stud.ntnu.no>
2025-01-18 11:15:17 +0100

    recorded test-data

---
+++ build_feather/zephyr/dev_handles.c
@@ -0,0 +1,81 @@
+#include <zephyr/device.h>
+#include <zephyr/toolchain.h>
+
+/* 1 : /soc/clock@40000000:
+ */
+const Z_DECL_ALIGN(device_handle_t) __attribute__((__section__(".__device_handles_pass2")))
+__devicehdl_dts_ord_38[] = { DEVICE_HANDLE_SEP, DEVICE_HANDLE_SEP, DEVICE_HANDLE_ENDS };
+
+/* 2 : /soc/gpio@50000300:
+ */
+const Z_DECL_ALIGN(device_handle_t) __attribute__((__section__(".__device_handles_pass2")))
+__devicehdl_dts_ord_10[] = { DEVICE_HANDLE_SEP, DEVICE_HANDLE_SEP, DEVICE_HANDLE_ENDS };
+
+/* 3 : /soc/gpio@50000000:
+ */
+const Z_DECL_ALIGN(device_handle_t) __attribute__((__section__(".__device_handles_pass2")))
+__devicehdl_dts_ord_48[] = { DEVICE_HANDLE_SEP, DEVICE_HANDLE_SEP, DEVICE_HANDLE_ENDS };
+
+/* 4 : /soc/random@4000d000:
+ */
+const Z_DECL_ALIGN(device_handle_t) __attribute__((__section__(".__device_handles_pass2")))
+__devicehdl_dts_ord_63[] = { DEVICE_HANDLE_SEP, DEVICE_HANDLE_SEP, DEVICE_HANDLE_ENDS };
+
+/* 5 : /soc/crypto@5002a000:
+ */
+const Z_DECL_ALIGN(device_handle_t) __attribute__((__section__(".__device_handles_pass2")))
+__devicehdl_dts_ord_81[] = { DEVICE_HANDLE_SEP, DEVICE_HANDLE_SEP, DEVICE_HANDLE_ENDS };
+
+/* 6 : /soc/usbd@40027000/cdc_acm_uart0:
+ */
+const Z_DECL_ALIGN(device_handle_t) __attribute__((__section__(".__device_handles_pass2")))
+__devicehdl_dts_ord_98[] = { DEVICE_HANDLE_SEP, DEVICE_HANDLE_SEP, DEVICE_HANDLE_ENDS };
+
+/* 7 : /soc/uart@40002000:
+ */
+const Z_DECL_ALIGN(device_handle_t) __attribute__((__section__(".__device_handles_pass2")))
+__devicehdl_dts_ord_77[] = { DEVICE_HANDLE_SEP, DEVICE_HANDLE_SEP, DEVICE_HANDLE_ENDS };
+
+/* 8 : /soc/timer@4000a000:
+ */
+const Z_DECL_ALIGN(device_handle_t) __attribute__((__section__(".__device_handles_pass2")))
+__devicehdl_dts_ord_7[] = { DEVICE_HANDLE_SEP, DEVICE_HANDLE_SEP, DEVICE_HANDLE_ENDS };
+
+/* 9 : /soc/i2c@40003000:
+ * Supported:
+ *    - /soc/i2c@40003000/mpu6050@69
+ */
+const Z_DECL_ALIGN(device_handle_t) __attribute__((__section__(".__device_handles_pass2")))
+__devicehdl_dts_ord_91[] = { DEVICE_HANDLE_SEP, DEVICE_HANDLE_SEP, 14, DEVICE_HANDLE_ENDS };
+
+/* 10 : /soc/flash-controller@4001e000:
+ */
+const Z_DECL_ALIGN(device_handle_t) __attribute__((__section__(".__device_handles_pass2")))
+__devicehdl_dts_ord_83[] = { DEVICE_HANDLE_SEP, DEVICE_HANDLE_SEP, DEVICE_HANDLE_ENDS };
+
+/* 11 : /soc/ecb@4000e000:
+ */
+const Z_DECL_ALIGN(device_handle_t) __attribute__((__section__(".__device_handles_pass2")))
+__devicehdl_dts_ord_40[] = { DEVICE_HANDLE_SEP, DEVICE_HANDLE_SEP, DEVICE_HANDLE_ENDS };
+
+/* 12 : /soc/radio@40001000/ieee802154:
+ */
+const Z_DECL_ALIGN(device_handle_t) __attribute__((__section__(".__device_handles_pass2")))
+__devicehdl_dts_ord_96[] = { DEVICE_HANDLE_SEP, DEVICE_HANDLE_SEP, DEVICE_HANDLE_ENDS };
+
+/* 13 : /soc/qspi@40029000/gd25q16@0:
+ */
+const Z_DECL_ALIGN(device_handle_t) __attribute__((__section__(".__device_handles_pass2")))
+__devicehdl_dts_ord_94[] = { DEVICE_HANDLE_SEP, DEVICE_HANDLE_SEP, DEVICE_HANDLE_ENDS };
+
+/* 14 : /soc/i2c@40003000/mpu6050@69:
+ * Direct Dependencies:
+ *    - /soc/i2c@40003000
+ */
+const Z_DECL_ALIGN(device_handle_t) __attribute__((__section__(".__device_handles_pass2")))
+__devicehdl_dts_ord_92[] = { 9, DEVICE_HANDLE_SEP, DEVICE_HANDLE_SEP, DEVICE_HANDLE_ENDS };
+
+/* 15 : /soc/temp@4000c000:
+ */
+const Z_DECL_ALIGN(device_handle_t) __attribute__((__section__(".__device_handles_pass2")))
+__devicehdl_dts_ord_71[] = { DEVICE_HANDLE_SEP, DEVICE_HANDLE_SEP, DEVICE_HANDLE_ENDS };
